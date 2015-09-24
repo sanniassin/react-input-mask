daecec5f00fbb619a521d021848c3f7edd62702b
sanniassin <nikitalobacev@Air.home>
2015-09-24 19:50:59 +0300

    react-input-mask 0.2.1

--- javascripts/InputElement.js
+++ javascripts/InputElement.js
@@ -4,6 +4,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 // https://github.com/sanniassin/react-input-mask
 
+
 var InputElement = React.createClass({
     displayName: "InputElement",
 
@@ -29,10 +30,14 @@ var InputElement = React.createClass({
 
         return input.getDOMNode();
     },
-    getPrefix: function getPrefix() {
+    getPrefix: function getPrefix(newState) {
         var prefix = "";
-        var mask = this.state.mask;
-        for (var i = 0; i < mask.length && this.isPermanentChar(i); ++i) {
+
+        var _ref = newState || this.state;
+
+        var mask = _ref.mask;
+
+        for (var i = 0; i < mask.length && this.isPermanentChar(i, newState); ++i) {
             prefix += mask[i];
         }
         return prefix;
@@ -97,15 +102,15 @@ var InputElement = React.createClass({
     formatValue: function formatValue(value, newState) {
         var _this2 = this;
 
-        var _ref = newState || this.state;
+        var _ref2 = newState || this.state;
 
-        var maskChar = _ref.maskChar;
-        var mask = _ref.mask;
+        var maskChar = _ref2.maskChar;
+        var mask = _ref2.mask;
 
         if (!maskChar) {
-            var prefixLen = this.getPrefix().length;
-            value = this.insertRawSubstr("", value, 0);
-            while (value.length > prefixLen && this.isPermanentChar(value.length - 1)) {
+            var prefixLen = this.getPrefix(newState).length;
+            value = this.insertRawSubstr("", value, 0, newState);
+            while (value.length > prefixLen && this.isPermanentChar(value.length - 1, newState)) {
                 value = value.slice(0, value.length - 1);
             }
             return value;
@@ -146,10 +151,10 @@ var InputElement = React.createClass({
         return value.slice(0, pos) + newSubstr + value.slice(pos + newSubstr.length);
     },
     insertRawSubstr: function insertRawSubstr(value, substr, pos, newState) {
-        var _ref2 = newState || this.state;
+        var _ref3 = newState || this.state;
 
-        var mask = _ref2.mask;
-        var maskChar = _ref2.maskChar;
+        var mask = _ref3.mask;
+        var maskChar = _ref3.maskChar;
 
         var isFilled = this.isFilled(value);
         substr = substr.split("");
@@ -178,10 +183,10 @@ var InputElement = React.createClass({
         return value;
     },
     getRawSubstrLength: function getRawSubstrLength(value, substr, pos, newState) {
-        var _ref3 = newState || this.state;
+        var _ref4 = newState || this.state;
 
-        var mask = _ref3.mask;
-        var maskChar = _ref3.maskChar;
+        var mask = _ref4.mask;
+        var maskChar = _ref4.maskChar;
 
         substr = substr.split("");
         for (var i = pos; i < mask.length && substr.length;) {
@@ -560,3 +565,4 @@ var InputElement = React.createClass({
         return React.createElement("input", _extends({ ref: "input" }, this.props, ourProps));
     }
 });
+
