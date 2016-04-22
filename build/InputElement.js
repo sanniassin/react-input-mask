43e8bdacea699ea4fcbd75f0f9798a271118693f
sanniassin <nikitalobacev@MacBook-Air-Nikita.local>
2016-04-22 16:43:14 +0300

    version 0.6.7

--- build/InputElement.js
+++ build/InputElement.js
@@ -410,7 +410,7 @@ var InputElement = React.createClass({
         this.permanents = mask.permanents;
         this.maskChar = "maskChar" in this.props ? this.props.maskChar : this.defaultMaskChar;
 
-        if (this.props.alwaysShowMask || value) {
+        if (this.mask && (this.props.alwaysShowMask || value)) {
             value = this.formatValue(value);
         }
 
@@ -434,6 +434,10 @@ var InputElement = React.createClass({
         this.mask = mask.mask;
         this.permanents = mask.permanents;
         this.maskChar = "maskChar" in nextProps ? nextProps.maskChar : this.defaultMaskChar;
+
+        if (!this.mask) {
+            return;
+        }
 
         var newValue = nextProps.value != null ? this.getStringValue(nextProps.value) : this.state.value;
 
