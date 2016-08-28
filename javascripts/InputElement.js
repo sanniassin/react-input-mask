34e381828924dbe72ce8228d91c8815e17e023b4
sanniassin <sanniassin@gmail.com>
2016-08-28 09:49:21 +0300

    react-input-mask 0.7.3

--- javascripts/InputElement.js
+++ javascripts/InputElement.js
@@ -4,6 +4,8 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 
 var InputElement = React.createClass({
     displayName: "InputElement",
