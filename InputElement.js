c1ea5bb010c1a70c71dbd6f4c0fe61093ed7337d
sanniassin <nikitalobacev@MacBook-Air-Nikita.local>
2017-02-02 12:50:15 +0300

    prevent cursor jump to start when mask changed on focus

--- InputElement.js
+++ InputElement.js
@@ -460,7 +460,13 @@ var InputElement = React.createClass({
                 var pos = this.lastCaretPos;
                 var filledLen = this.getFilledLength(newValue);
                 if (pos === null || filledLen < pos) {
-                    this.setCaretPos(this.getRightEditablePos(filledLen));
+                    if (this.isFilled(newValue)) {
+                        pos = filledLen;
+                    }
+                    else {
+                        pos = this.getRightEditablePos(filledLen);
+                    }
+                    this.setCaretPos(pos);
                 }
             }
         }
