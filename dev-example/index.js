2cc4f384694969fb92b01e027de1873d84c60621
sanniassin <nikitalobacev@MacBook-Air-Nikita.local>
2017-06-27 16:15:25 +0300

    print console output on dev page

--- dev-example/index.js
+++ dev-example/index.js
@@ -17,3 +17,19 @@ class Input extends React.Component {
 }
 
 ReactDOM.render(<Input />, document.getElementById('root'));
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+var consoleDiv = document.getElementById('console');
+var log = console.log;
+console.log = (text, ...rest) => {
+  log.apply(console, [text, ...rest]);
+  consoleDiv.innerHTML = `${escapeHtml(text)}<br/>${consoleDiv.innerHTML}`;
+};
