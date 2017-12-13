61f9f8cef6e447c73e4554ff0e9f2b6036a4c6d9
Nikita Lobachev <sanniassin@gmail.com>
2017-12-13 18:10:10 +0300

    Omit unnecessary devServer settings

--- webpack.config.js
+++ webpack.config.js
@@ -39,9 +39,7 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: 9000,
-    disableHostCheck: true,
-    hot: false,
-    inline: false
+    disableHostCheck: true
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
