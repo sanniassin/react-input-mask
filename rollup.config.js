f1e8720417d4394c8d9dbd0e91471c98c2340fc2
Nikita Lobachev <sanniassin@gmail.com>
2018-05-19 11:53:56 +0300

    Update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
