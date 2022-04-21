d0750dc3d0bfe24c2add45ac5d4510d3c1150464
André <abs@emancipe.se>
2022-04-21 16:33:57 -0300

    update rollup plugins to their new official packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import { babel } from "@rollup/plugin-babel"
 import { terser } from "rollup-plugin-terser";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import replace from "rollup-plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 import protoToAssign from "./rollup.proto-to-assign.plugin";
 
 const input = "./src/index.js";
