28aeb1b69b27145f9d473e012ba0401653dbfdaa
André <abs@emancipe.se>
2022-04-21 15:56:57 -0300

    change eslint to setup to use *recommended plugins only

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,14 @@
-const OFF = 0;
-const WARN = 1;
-const ERROR = 2;
-
 module.exports = {
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  extends: ["airbnb", "prettier"],
-  plugins: ["prettier", "react-hooks"],
-  env: {
+  root: true,
+  parser: "@babel/eslint-parser",
+  extends: ["eslint:recommended", "plugin:import/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended"],
+/*  env: {
     browser: true
   },
-  rules: {
-    "react/jsx-filename-extension": OFF,
-    "react/jsx-props-no-spreading": OFF,
-    "react/require-default-props": OFF,
-    'react/no-find-dom-node': OFF,
-    "react/prop-types": [ERROR, { ignore: ["value", "defaultValue"] }],
-    "react-hooks/rules-of-hooks": ERROR,
-    "react-hooks/exhaustive-deps": ERROR,
-    "no-shadow": OFF,
-    "no-param-reassign": OFF,
-    "no-plusplus": OFF,
-    "global-require": OFF,
-    "consistent-return": OFF,
-    "prefer-const": [ERROR, {
-        "destructuring": "all"
-    }],
-    "prettier/prettier": ERROR,
+*/
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 };
